0fa01a74c1a34e738cebc9ec3ddc53698e32dcbd
Adrian Hirt <aedu.hirt@gmail.com>
2025-12-21 10:33:28 +0100

    Debug

--- apps/vulkan_app/main.cpp
+++ apps/vulkan_app/main.cpp
@@ -7,6 +7,7 @@ public:
 
   Model cube1 = ModelFactory::createCube({0.0f, 1.0f, 0.0f});
   Model cube2 = ModelFactory::createCube({0.0f, 1.0f, 0.0f});
+  // Model sphere = ModelFactory::createSphere();
 
   Model floor = ModelFactory::createGroundPlane(10);
   Model cube = ModelFactory::createCube({0.0f, 1.0f, 0.0f});
@@ -17,6 +18,7 @@ public:
   SceneNode floor_node = SceneNode(&floor);
   SceneNode cube1_node = SceneNode(&cube1);
   SceneNode cube2_node = SceneNode(&cube2);
+  // SceneNode sphere_node = SceneNode(&sphere);
 
   bool forward = true;
 
@@ -27,12 +29,15 @@ public:
     root_node.addChildNode(&cube1_node);
     root_node.addChildNode(&cube2_node);
     root_node.addChildNode(&floor_node);
+    // root_node.addChildNode(&sphere_node);
 
     // Update initial transforms
     cube1_node.setScale(0.1f, 0.1f, 0.1f);
     cube2_node.setScale(0.1f, 0.1f, 0.1f);
     cube1_node.setPosition(0.0f, 1.5f, -2.0f);
     cube2_node.setPosition(0.0f, 1.5f, 0.0f);
+    // sphere_node.scale(0.1f, 0.1f, 0.1f);
+    // sphere_node.setPosition(0.0f, 1.5f, 0.0f);
     root_node.updateTransformation();
 
     cube1.toggleRenderBoundingBoxes();
